058b4434ba33efbdedc1b494844c5ad9463b64a1
rjp301 <rileypaul96@gmail.com>
2020-10-15 16:07:38 -0700

    updated weekly plot script

--- WEATHER_plot_week.py
+++ WEATHER_plot_week.py
@@ -7,9 +7,18 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 from wunderground_pws import WUndergroundAPI, units
+from sys import platform
+import dropbox
+
+def upload_to_dbx(fname_in,fname_out):
+    app_token = "C3UXpmLsHncAAAAAAAAAAXrgdTMQG1hC3tiAmQJSRxzDm432bzXP8MSpfwoectAX"
+    dbx = dropbox.Dropbox(app_token)
+
+    with open(fname_in,"rb") as file:
+        dbx.files_upload(file.read(),fname_out,mode=dropbox.files.WriteMode.overwrite)
 
 wu = WUndergroundAPI(
-    api_key="6c21a0c6b224472ba1a0c6b224372b63",
+    api_key="1e81c4e2de6e476e81c4e2de6e476e21",
     units=units.METRIC_UNITS)
 
 class PWS(object):
@@ -25,14 +34,13 @@ stations = [
     # PWS("SAEG_Stuart","IBULKL8","xKoizTAr"),
     # PWS("SAEG_Anzac","IREGIO58","d6MfuO8f")]
 
-fname = "weather\\20yr_record\\CGL - Weather Summary - Vanderhoof - 1098D90.xlsx"
-data = pd.read_excel(fname,index_col=0)
+if platform.startswith('linux'): fname_data = "/home/pi/weather/DATA_historical/CGL - Weather Summary - Vanderhoof - 1098D90.xlsx"
+else: fname_data = "DATA_historical\\CGL - Weather Summary - Vanderhoof - 1098D90.xlsx"
+
+data = pd.read_excel(fname_data,index_col=0)
 print(data)
 
-
-
-
-today = ts("2020-09-10")
+today = ts("2020-10-07")
 dates = [today + td(days=i) for i in range(0-today.weekday(),7-today.weekday())]
 labels = [date.strftime("%Y-%m-%d") for date in dates]
 
@@ -40,22 +48,29 @@ actual = [wu.history(date=date,granularity="daily",station_id=stations[0].ID)["o
 average = [data["Max"][date] for date in dates]
 week_start = dates[0].strftime("%Y-%m-%d")
 
+if platform.startswith('linux'): fname_fig = f"/home/pi/weather/PLOT_Weekly_Rainfall - {week_start}.png"
+else: fname_fig = f"PLOT_Weekly_Rainfall - {week_start}.png"
+
 plt.plot(dates,actual,label="Actual",color="red",marker="o")
 plt.plot(dates,average,label="10-year Maximum",color="gray",marker="o")
 
 plt.grid(axis="y")
 
-leg = plt.legend(labels, bbox_to_anchor=(0.,1.15,1.,.102),prop ={'size':10},loc=10,ncol=2,title=f"Weekly Rainfall Summary for Week of {week_start}")                                         
+leg_labels = ["Current (IVANDE4)","10yr Historical Max (1098D90)"]
+leg = plt.legend(leg_labels, bbox_to_anchor=(0.,1.15,1.,.102),prop ={'size':10},loc=10,ncol=2,title=f"Weekly Rainfall Summary for Week of {week_start}")                                         
 leg.get_title().set_fontsize('12') 
 
-
-# plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',ncol=2,mode="expand", borderaxespad=0.)
 plt.ylabel("Total Rain (mm)")
-# plt.xlabel(f"Weekly Rainfall Summary for Week of {week_start}",fontsize=12,labelpad=10)
 plt.xticks(ticks=dates,labels=labels)
 plt.tight_layout()
 
 fig = plt.gcf()
 fig.set_size_inches(8,4)
-fig.savefig(f"weather\\Weekly_Rainfall_{week_start}.png",dpi=100)
+fig.savefig(fname_fig,dpi=100)
 plt.show()
+
+fname_out = f"/5_ENG-CGLS34/42_Weather/PLOT_Weekly_Rainfall - {week_start}.png"
+upload_to_dbx(fname_fig,fname_out)
+print("File has been uploaded")
+os.remove(fname_fig)
+print("File has been deleted")

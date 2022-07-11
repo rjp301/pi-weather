ac828326642fb6688f50c3b04318a044147e30c9
Riley Paul <rileypaul96@gmail.com>
2022-07-11 09:16:31 -0600

    removed styling to seperate html file and cleaned up filepath handling

--- send_email.py
+++ send_email.py
@@ -1,13 +1,13 @@
 import sendgrid
 from sendgrid.helpers.mail import *
 
-def send_email(emails):
+def send_email(emails,subject,html):
   sg = sendgrid.SendGridAPIClient(api_key="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")
+  
   from_email = Email("saeg.weather@gmail.com")
-  to_email = To("rileypaul96@gmail.com")
-  subject = "Sending with SendGrid is Fun"
-  content = Content("text/plain", "and easy to do anywhere, even with Python")
-  mail = Mail(from_email, to_email, subject, content)
+  to_email = To(emails)
+  mail = Mail(from_email, to_email, subject, html_content=html)
+
   response = sg.client.mail.send.post(request_body=mail.get())
   print(response.status_code)
   print(response.body)

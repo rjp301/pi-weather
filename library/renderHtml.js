758a8365ac1e46ebfc8a2be3b09dbced859ce75e
Riley Paul <rileypaul96@gmail.com>
2023-03-30 15:04:43 -0700

    add overall header to summary

--- library/renderHtml.js
+++ library/renderHtml.js
@@ -5,6 +5,9 @@ const source = await fs.readFile("./html/summary_template.html","utf-8")
 const template = Handlebars.compile(source)
 
 const data = JSON.parse(await fs.readFile("./data/weatherSummary.json"))
+data["len_hrs"] = data.columns.filter(i => i.includes("Temp")).length
+data["len_rng"] = data.columns.filter(i => i.includes("-")).length
+
 const result = template(data)
 
 await fs.writeFile("./html/summary.html",result)

1182e9a2d0661ef6ee7db9be558e7ae742d81e75
Riley Paul <rileypaul96@gmail.com>
2023-04-11 07:15:52 -0700

    begin converting to all JS

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import fetchWeatherData from "./library/fetchWeatherData.js";
+import summarizeData from "./library/summarizeData.js";
 
-
+let { observations: data } = await fetchWeatherData("IREGIO61");
 
 console.table(data);
+console.table(summarizeData(data));
--- library/summarizeData.js
+++ library/summarizeData.js
@@ -1,18 +1,24 @@
 const TzString = "Canada/Pacific"
 
 function roundMinutes(date) {
-  date.setHours(date.getHours() + Math.round(date.getMinutes() / 60));
-  date.setMinutes(0, 0, 0); // Resets also seconds and milliseconds
-  return date;
+    date.setHours(date.getHours() + Math.round(date.getMinutes() / 60));
+    date.setMinutes(0, 0, 0); // Resets also seconds and milliseconds
+    return date;
+  }
+
+export default function summarizeData(data,options) {
+  const result = {}
+
+
+  
+  // hrs_of_interest = [7,13,19]
+  // rng_of_interest = [(5,17),(17,29),(0,24)]
+  // yesterday = dt.date.today() - dt.timedelta(days=1)
+  
+  data = data.map((obs) => ({
+    ...obs,
+    obsTimeRnd: roundMinutes(new Date(obs.obsTimeUtc)),
+  }));
+
+  return result
 }
-
-// hrs_of_interest = [7,13,19]
-// rng_of_interest = [(5,17),(17,29),(0,24)]
-// yesterday = dt.date.today() - dt.timedelta(days=1)
-
-let { observations: data } = await fetchWeatherData("IREGIO61");
-
-data = data.map((obs) => ({
-  ...obs,
-  obsTimeRnd: roundMinutes(new Date(obs.obsTimeUtc)),
-}));

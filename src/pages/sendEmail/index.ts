3af743aff467f9b389daa347d967ec6ec04ee954
Riley Paul <rileypaul96@gmail.com>
2023-08-23 23:43:31 -0700

    add adapter

--- src/pages/sendEmail/index.ts
+++ src/pages/sendEmail/index.ts
@@ -1,8 +1,6 @@
 import type { APIRoute } from "astro";
-import summarizeStations from "@/lib/summarizeStations";
 import sendEmail from "@/lib/sendEmail";
 import { DateTime } from "luxon";
-import { getCollection, getEntry } from "astro:content";
 
 export const post: APIRoute = async ({ url, params, redirect }) => {
   const test = Boolean(url.searchParams.get("test"));
@@ -12,25 +10,17 @@ export const post: APIRoute = async ({ url, params, redirect }) => {
   const date = DateTime.fromISO(dateString);
   if (date.invalidReason) return new Response(null, { status: 404 });
 
-  const stations = await getCollection("stations");
-  const timesOfInterest = await getEntry("timesOfInterest", "timesOfInterest");
-
-  const summary = await summarizeStations(
-    stations.map((station) => station.data),
-    date,
-    timesOfInterest.data
+  const html = await fetch(`${url.origin}/${dateString}/raw`).then((res) =>
+    res.text()
   );
-
-  const html = await fetch(`${url.origin}/${dateString}/raw`).then(res => res.text())
-  console.log(html)
 
   const subject = `CGL S34 Weather Summary - ${date.toFormat("yyyy-LL-dd")}`;
 
   try {
     await sendEmail(subject, html, true);
-    return redirect(`/sendEmail/sent?test=${test}&date=${dateString}`);
+    return redirect(`/sendEmail/sent?${url.searchParams.toString()}`);
   } catch {
     console.log("Could not send email");
-    return redirect(`/sendEmail/fail?test=${test}&date=${dateString}`);
+    return redirect(`/sendEmail/fail?${url.searchParams.toString()}`);
   }
 };

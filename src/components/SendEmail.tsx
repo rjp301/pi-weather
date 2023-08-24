ed17af18351fa44a5352b1c8ec5bc4acc502532b
Riley Paul <rileypaul96@gmail.com>
2023-08-24 06:33:06 -0700

    convert all links to use query params

--- src/components/SendEmail.tsx
+++ src/components/SendEmail.tsx
@@ -2,6 +2,7 @@ import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { useState } from "react";
 import { Button } from "./ui/button";
+import axios from "axios";
 
 interface Props {
   dateString: string;
@@ -11,16 +12,14 @@ export function SendEmail(props: Props) {
   const { dateString } = props;
   const [test, setTest] = useState(true);
   return (
-    <form
-      action={`${Astro.url.origin}/sendEmail/?date=${dateString}&test=${test}`}
-      method="post"
-      className="flex gap-4"
-    >
+    <div className="flex gap-4">
       <div className="flex items-center space-x-2">
         <Switch checked={test} onCheckedChange={(e) => setTest(e.valueOf())} />
         <Label>Test Email</Label>
       </div>
-      <Button>Email to Recipients</Button>
-    </form>
+      <a href={`/sendEmail?date=${dateString}&test=${test}`}>
+        <Button>Email to Recipients</Button>
+      </a>
+    </div>
   );
 }
--- src/pages/sendEmail/index.ts
+++ src/pages/sendEmail/index.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import sendEmail from "@/lib/sendEmail";
 import { DateTime } from "luxon";
 
-export const post: APIRoute = async ({ url, params, redirect }) => {
+export const get: APIRoute = async ({ url, params, redirect }) => {
   const test = Boolean(url.searchParams.get("test"));
   const dateString = url.searchParams.get("date");
   if (!dateString) return new Response(null, { status: 404 });
@@ -10,8 +10,8 @@ export const post: APIRoute = async ({ url, params, redirect }) => {
   const date = DateTime.fromISO(dateString);
   if (date.invalidReason) return new Response(null, { status: 404 });
 
-  const html = await fetch(`${url.origin}/${dateString}/raw`).then((res) =>
-    res.text()
+  const html = await fetch(`${url.origin}/summary/raw?date=${dateString}`).then(
+    (res) => res.text()
   );
 
   const subject = `CGL S34 Weather Summary - ${date.toFormat("yyyy-LL-dd")}`;

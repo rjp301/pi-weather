7ba9753f56f5f2fb9c43a91fbf82da28871911b2
Riley Paul <rileypaul96@gmail.com>
2023-08-23 20:11:46 -0700

    move times of interest to json

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -17,4 +17,12 @@ const stations = defineCollection({
   }),
 });
 
-export const collections = { emails, stations };
+const timesOfInterest = defineCollection({
+  type: "data",
+  schema: z.object({
+    hours: z.array(z.number()),
+    ranges: z.array(z.object({ beg: z.number(), end: z.number() })),
+  }),
+});
+
+export const collections = { emails, stations, timesOfInterest };

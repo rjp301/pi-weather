852208f3bd7f5ac5932f2768d137dcd0052b2450
Riley Paul <rileypaul96@gmail.com>
2023-08-25 17:46:03 -0700

    email adding and deleting

---
+++ src/pages/api/emails/[id]/delete.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from "astro";
+
+export const post: APIRoute = async ({ locals, params, redirect }) => {
+  const { id } = params;
+  if (!id) return new Response(null, { status: 404 });
+
+  try {
+    await locals.pb.collection("emails").delete(id);
+    return redirect("/emails");
+  } catch (err) {
+    return new Response(JSON.stringify(err), { status: 500 });
+  }
+};
---
+++ src/pages/api/emails/index.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+
+export const post: APIRoute = async ({ locals, url, redirect, request }) => {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  try {
+    await locals.pb
+      .collection("emails")
+      .create({ user: locals.user.id, email });
+    return redirect("/emails");
+  } catch (err) {
+    return new Response(JSON.stringify(err), { status: 500 });
+  }
+};

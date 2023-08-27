a5d9d7d94757cc61ea88d8871d4c53d0162bfec5
Riley Paul <rileypaul96@gmail.com>
2023-08-27 09:17:40 -0700

    update logo and navbar

--- src/pages/api/emails/index.ts
+++ src/pages/api/emails/index.ts
@@ -4,10 +4,11 @@ import type { APIRoute } from "astro";
 export const post: APIRoute = async ({ locals, url, redirect, request }) => {
   const formData = await request.formData();
   const email = formData.get("email");
+  const tester = formData.get("tester") === "on";
   try {
     await locals.pb
       .collection("emails")
-      .create({ user: locals.user.id, email });
+      .create({ user: locals.user.id, email, tester });
     return redirect(`/emails?datareload=${randomString()}`);
   } catch (err) {
     return new Response(JSON.stringify(err), { status: 500 });

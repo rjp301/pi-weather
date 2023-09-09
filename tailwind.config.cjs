d6b192e8138c4e1ea5597213852f98fe28f8e84d
Riley Paul <rileypaul96@gmail.com>
2023-09-09 06:58:26 -0700

    fix issue with dar kmode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./src/**/*.{ts,tsx,astro}"],
   theme: {
     container: {

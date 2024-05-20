4698273935a4b072898b8f2466ef08981a511ea8
Riley Paul <rileypaul96@gmail.com>
2024-05-19 20:55:16 -0600

    copy some api files  over

--- src/api/db/schema.ts
+++ src/api/db/schema.ts
@@ -51,3 +51,29 @@ export type Summary = typeof summaries.$inferSelect;
 export type SummaryInsert = typeof summaries.$inferInsert;
 export const summarySchema = createSelectSchema(summaries);
 export const summaryInsertSchema = createInsertSchema(summaries);
+
+export const userTable = sqliteTable("user", {
+  id: text("id").$defaultFn(uuid).primaryKey().unique(),
+  githubId: integer("github_id").unique(),
+  username: text("username").notNull(),
+  name: text("name").notNull(),
+  avatarUrl: text("avatar_url"),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+});
+
+export type User = typeof userTable.$inferSelect;
+
+export const sessionTable = sqliteTable("user_session", {
+  id: text("id").notNull().primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: integer("expires_at").notNull(),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+});
+
+export type Session = typeof sessionTable.$inferSelect;

53ce9d51d10c28e858c6565f01adcb2746a97d3d
Riley Paul <rileypaul96@gmail.com>
2024-05-20 17:34:18 -0600

    move some files around and improve header

---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { User as UserIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import type { User } from "@/api/db/schema";
+
+interface Props {
+  user: User | null;
+}
+
+const UserAvatar: React.FC<Props> = ({ user }) => {
+  if (!user) {
+    return (
+      <a className={cn(buttonVariants())} href="/api/auth/login/github">
+        <GitHubLogoIcon className="mr-2" />
+        Login with GitHub
+      </a>
+    );
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src={user.avatarUrl ?? ""} />
+          <AvatarFallback>
+            <UserIcon />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
+        <a href="/api/auth/logout">
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </a>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserAvatar;

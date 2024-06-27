e95f605af8cf12a8f6b125b02e8da0da33b25533
Riley Paul <rileypaul96@gmail.com>
2024-06-27 11:46:41 -0600

    remove vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
 import node from "@astrojs/node";
 
 // https://astro.build/config

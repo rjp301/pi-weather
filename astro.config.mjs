2c244bcd104fe14d70a763432f6583f828363a4e
Riley Paul <rileypaul96@gmail.com>
2024-07-17 08:29:36 -0600

    upgrade astro and self host font awesome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), db()],
   adapter: node({
     mode: "standalone"
   })
